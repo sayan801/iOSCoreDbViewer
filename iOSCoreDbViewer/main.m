8fc9255f31a3a3d4c65e7f0e766fb215c10f643b
Agnit Sarkar <Agnit@agnits-imac.mshome.net>
2015-03-25 21:49:12 +0530

    Initial Commit

---
+++ iOSCoreDbViewer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSCoreDbViewer
+//
+//  Created by Agnit Sarkar on 25/03/15.
+//  Copyright (c) 2015 technicise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
